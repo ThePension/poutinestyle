e41089745657a4e7365edcc63b77f3802992ff1e
alessio.comi <alessio.comi@he-arc.ch>
2021-10-06 13:28:32 +0200

    Initial commit

--- PoutineStyle/main.cpp
+++ PoutineStyle/main.cpp
@@ -12,8 +12,8 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
+    sf::CircleShape shape(150.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
