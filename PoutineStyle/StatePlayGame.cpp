86c732ac1d50836be4037c586dcc1af9e5c13437
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2021-10-06 13:31:31 +0200

    Add classes in project

---
+++ PoutineStyle/StatePlayGame.cpp
@@ -0,0 +1,1 @@
+#include "StatePlayGame.h"
--- PoutineStyle/main.cpp
+++ PoutineStyle/main.cpp
@@ -8,27 +8,12 @@
 * Description   : Remake of Wolfenstein 3D with raycasting
 */
 
-#include <SFML/Graphics.hpp>
+#include "GameManager.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    GameManager game;
+    game.gameLoop();
 
     return 0;
 }
