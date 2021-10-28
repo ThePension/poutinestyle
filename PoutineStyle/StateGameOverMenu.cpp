d6c7a6f6bb12d93f5b807a33034fc8726c2c58d2
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2021-10-28 19:25:27 +0200

    Adding StateGameOverMenu class

---
+++ PoutineStyle/StateGameOverMenu.cpp
@@ -0,0 +1,1 @@
+#include "StateGameOverMenu.h"
---
+++ PoutineStyle/StateGameOverMenu.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameState.h"
+#include "StatePlayGame.h"
+#include "Button.h"
+class StateGameOverMenu : public GameState
+{
+private:
+	Button replayButton;
+	Button quitButton;
+
+protected:
+	void handleInput() override;
+	void draw() override;
+
+public:
+	StateGameOverMenu(GameManager* game);
+	~StateGameOverMenu();
+	void update() override;
+};
+
