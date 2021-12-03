e52787ec96eec44b8cc314ed055dedefba739dfe
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2021-12-03 09:18:16 +0100

    Updating Animation class

--- PoutineStyle/Animation.cpp
+++ PoutineStyle/Animation.cpp
@@ -1,8 +1,44 @@
 #include "Animation.h"
-Animation::Animation(int x, int y, int width, int height, int frameCount, std::string texturePath) {
+
+Animation::Animation() { }
+
+Animation::Animation(int x, int y, int width, int height, int frameCount, std::string texturePath, float holdTime) {
 	texture.loadFromFile(texturePath);
 	this->nbFrames = frameCount;
+	this->frameDuration = holdTime;
+	frames.resize(frameCount);
 	for (int i = 0; i < nbFrames; i++) {
-		frames[i] = sf::IntRect(x + i * width, y + i * 0, width, height);
+		this->frames[i] = sf::IntRect(x + i * width, y + i * 0, width, height);
+	}
+}
+
+Animation::~Animation()
+{
+
+}
+
+void Animation::update(float dt)
+{
+	isAnimationOver = false;
+	time += dt;
+	while (time >= frameDuration)
+	{
+		time -= frameDuration;
+		advance();
+	}
+}
+
+int Animation::ApplyToSprite(sf::Sprite& sprite)
+{
+	sprite.setTexture(texture);
+	sprite.setTextureRect(frames[currentRenderedFrameNum]);
+	return currentRenderedFrameNum;
+}
+
+void Animation::advance()
+{
+	if (++currentRenderedFrameNum >= nbFrames) {
+		currentRenderedFrameNum = 0;
+		isAnimationOver = true;
 	}
 }
--- PoutineStyle/Animation.h
+++ PoutineStyle/Animation.h
@@ -3,40 +3,19 @@
 class Animation
 {
 public:
-	Animation() {}
-	Animation(int x, int y, int width, int height, int frameCount, std::string texturePath);
-
-	int ApplyToSprite(sf::Sprite& sprite)
-	{
-		sprite.setTexture(texture);
-		sprite.setTextureRect(frames[currentRenderedFrameNum]);
-		return currentRenderedFrameNum;
-	}
-
-	void update(float dt)
-	{
-		isAnimationOver = false;
-		time += dt;
-		while (time >= frameDuration)
-		{
-			time -= frameDuration;
-			advance();
-		}
-	}
+	Animation();
+	Animation(int x, int y, int width, int height, int frameCount, std::string texturePath, float holdTime = 0.1f);
+	~Animation();
+	int ApplyToSprite(sf::Sprite& sprite);
+	void update(float dt);
 	bool isAnimationOver = true;
 private:
-	void advance()
-	{
-		if (++currentRenderedFrameNum >= nbFrames) {
-			currentRenderedFrameNum = 0;
-			isAnimationOver = true;
-		}
-	}
+	void advance();
 
 	int nbFrames = 4;
 	int currentRenderedFrameNum = 0;
 	sf::Texture texture;
-	sf::IntRect* frames;
+	std::vector<sf::IntRect> frames;
 	float frameDuration = 0.1f;
 	float time = 0;
 };
