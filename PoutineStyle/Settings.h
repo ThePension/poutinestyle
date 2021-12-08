2d2b4d66768773791c331e07325789f830b9ceb2
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2021-12-08 13:20:40 +0100

    Implemenet class settings

--- PoutineStyle/Settings.h
+++ PoutineStyle/Settings.h
@@ -2,5 +2,26 @@
 
 class Settings
 {
+	public:
+		Settings();
+		~Settings();
 
+		int getLevel();
+		bool getShowMetaData();
+		int getSensibility();
+		int getDifficulty();
+		int getVolume();
+
+		void setLevel(int level);
+		void setShowMetaData(bool showMetaData);
+		void setSensibility(int sensibility);
+		void setDifficulty(int difficulty);
+		void setVolume(int volume);
+
+	private:
+		int level;
+		bool showMetaData;
+		int sensibility;
+		int difficulty;
+		int volume;
 };
