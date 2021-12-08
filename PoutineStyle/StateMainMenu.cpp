e7b8f5101c1010f6339fd5c6e9a0ae1245cb0223
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2021-12-08 13:10:12 +0100

    nothing

--- PoutineStyle/StateMainMenu.cpp
+++ PoutineStyle/StateMainMenu.cpp
@@ -6,7 +6,7 @@ StateMainMenu::StateMainMenu(GameManager * game) {
 	int w = gameManager->getWindowWidth();
 	int h = gameManager->getWindowHeight();
 	playButton = Button(sf::Vector2i(w / 2 - 75, 1*(h / 4) - 25), 150, 50, sf::Color::Cyan, L"Jouer", gameManager->getRenderWindow());
-	settingsButton = Button(sf::Vector2i(w / 2 - 75, 2 * (h / 4) - 25), 150, 50, sf::Color::Cyan, L"Paramètres", gameManager->getRenderWindow());
+	settingsButton = Button(sf::Vector2i(w / 2 - 75, 2 * (h / 4) - 25), 150, 50, sf::Color::Cyan, L"Parametres", gameManager->getRenderWindow());
 	quitButton = Button(sf::Vector2i(w / 2 - 75, 3 * (h / 4) - 25), 150, 50, sf::Color::Cyan, L"Quitter", gameManager->getRenderWindow());
 }
 StateMainMenu::~StateMainMenu() {
