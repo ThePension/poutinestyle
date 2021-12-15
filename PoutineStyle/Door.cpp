e86826769a2931ddd6b5bed5445061c04a63bb84
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2021-12-15 10:47:49 +0100

    Adding Door class and update Wall class

---
+++ PoutineStyle/Door.cpp
@@ -0,0 +1,37 @@
+#include "Door.h"
+
+Door::Door(sf::Vector2f pos, int frameCount, int y, double frameDuration, bool isDestructible, bool isSecretPassage)
+	: Wall(pos, frameCount, y, frameDuration, isDestructible)
+{
+	this->isSecretPassage = isSecretPassage;
+	this->isTransparent = false;
+}
+
+Door::~Door()
+{
+	// Nothing to delete
+}
+
+void Door::update(float dt)
+{
+	if (isOpening) {
+		isAnimationOver = false;
+		time += dt;
+		while (time >= frameDuration)
+		{
+			time -= frameDuration;
+			if (++currentTextureCoordinates.x >= frameCount) {
+				this->toRemove = true;
+				currentTextureCoordinates.x = 0;
+				isAnimationOver = true;
+			}
+		}
+	}
+}
+
+void Door::setOpening()
+{
+	this->isOpening = true;
+	this->isTransparent = true;
+	if (isSecretPassage) this->currentTextureCoordinates.y += 1;
+}
